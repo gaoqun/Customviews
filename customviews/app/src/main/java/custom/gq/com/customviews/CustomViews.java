11ad06e38a77e4c5b04c16957826ab16146b4e36
Gaige <gaoqun@wiseweb.com.cn>
2016-06-16 18:16:23 +0800

    添加贝塞尔曲线例子 添加path操作

--- customviews/app/src/main/java/custom/gq/com/customviews/CustomViews.java
+++ customviews/app/src/main/java/custom/gq/com/customviews/CustomViews.java
@@ -2,18 +2,21 @@ package custom.gq.com.customviews;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Picture;
 import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.Toast;
 
 /**
  * Created by gaoqun on 2016/6/14.
@@ -76,7 +79,7 @@ public class CustomViews extends View{
     @Override
     protected void onDraw(final Canvas canvas) {
 //        mPicture.draw(canvas);不推荐使用 低版本不兼容
-        canvas.drawPicture(mPicture);
+//        canvas.drawPicture(mPicture);
 
         mPaint.setStyle(Paint.Style.STROKE);
 
@@ -87,7 +90,7 @@ public class CustomViews extends View{
 
         canvas.save();
         canvas.translate(200, 200);
-        canvas.drawRect(sRect, mPaint);
+        /*canvas.drawRect(sRect, mPaint);
         for (int i = 0; i < 200; i++) {
             if (i % 2 == 0)
                 canvas.drawPoint(0, i, mPaint);
@@ -150,6 +153,94 @@ public class CustomViews extends View{
         canvas.drawLine(100, 100, 300, 300, mPaint);
         canvas.drawRoundRect(100, 100, 400, 400, 30, 30, mPaint);
         Toast.makeText(getContext(), "" + canvas.getSaveCount(), Toast.LENGTH_LONG).show();
+
+//        canvas.drawPicture(mPicture);
+        PictureDrawable pictureDrawable = new PictureDrawable(mPicture);
+        pictureDrawable.draw(canvas);*/
+        Bitmap bitmap = null;
+        //读取资源文件
+//        bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_eg);
+        //        canvas.drawBitmap(bitmap,10,10,mPaint);
+
+        //读取assets图片
+//        try {
+//            InputStream inputStream = getContext().getAssets().open("ic_eg.jpg");
+//            bitmap = BitmapFactory.decodeStream(inputStream);
+////            bitmap.setConfig(Bitmap.Config.RGB_565);
+////            bitmap.getScaledWidth(canvas);
+//            canvas.drawBitmap(bitmap,10,10,mPaint);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        //读取内存卡读片
+        bitmap = BitmapFactory.decodeFile("/storage/emulated/0/Download/190550.jpg");
+//        canvas.drawBitmap(bitmap,200,200,mPaint);
+        //网络资源读取 略
+
+        Rect src = new Rect(0,0,bitmap.getWidth()/2,bitmap.getHeight()/2);
+        Rect dst = new Rect(0,0,300,300);
+        canvas.drawBitmap(bitmap,src,dst,mPaint);
+        canvas.save();
+        canvas.restore();
+        canvas.translate(300,300);
+        mPaint.setColor(Color.RED);
+        mPaint.setAlpha(150);
+        mPaint.setARGB(100,100,100,100);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(2);
+        mPaint.setTextSize(100);
+        mPaint.setTypeface(Typeface.MONOSPACE);//字体样式
+        mPaint.setTextAlign(Paint.Align.CENTER);//设置对其样式
+
+
+        canvas.drawText("fuck",0,0,mPaint);
+        canvas.drawText("abccdefg",2,5,100,100,mPaint);
+
+        Path path = new Path();
+        path.lineTo(0,300);
+        if(!path.getFillType().equals(Path.FillType.WINDING)){
+            path.setFillType(Path.FillType.INVERSE_EVEN_ODD);
+        }
+        path.rLineTo(200,100);
+        path.close();
+        canvas.drawPath(path,mPaint);
+        mPaint.setAlpha(255);
+        path.moveTo(100,100);
+        path.setLastPoint(0,0);
+        path.addOval(0,0,300,300, Path.Direction.CW);
+        canvas.drawPath(path,mPaint);
+        mPaint.setTextAlign(Paint.Align.RIGHT);
+        mPaint.setTypeface(Typeface.MONOSPACE);
+        path.addRect(200,200,500,500, Path.Direction.CCW);
+        canvas.drawPath(path,mPaint);
+//        if (!path.isEmpty())
+//        path.addPath(path);
+        mPaint.setStrokeWidth(10);
+        path.addArc(new RectF(0,0,100,100),30,160);
+        canvas.drawPath(path,mPaint);
+        Path path1 = new Path();
+        RectF rect = new RectF(-200,-199,400,500);
+        path1.addRect(rect, Path.Direction.CW);
+        path1.rCubicTo(100,100,300,300,500,300);
+        path1.incReserve(5);
+//        path.set(path1);
+//        path.op(path1, Path.Op.UNION);//op combine xor
+        path.op(path1, Path.Op.REVERSE_DIFFERENCE);//op combine xor
+        canvas.drawPath(path,mPaint);
+        path.reset();
+        path.lineTo(300,0);
+        path.lineTo(200,0);
+        path.close();
+        path.computeBounds(rect,true);//极端边界值
+        path.transform(new Matrix());
+        canvas.drawPath(path,mPaint);
+        path.setLastPoint(200,100);//reset last point
+        path.addRect(rect, Path.Direction.CCW);//逆时针  CW顺指针
+
+        //贝塞尔曲线
+
+
+
 
 
 
--- customviews/app/src/main/java/custom/gq/com/customviews/MainActivity.java
+++ customviews/app/src/main/java/custom/gq/com/customviews/MainActivity.java
@@ -2,9 +2,8 @@ package custom.gq.com.customviews;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,16 +11,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.rl);
+        CubicCustomView cubicCustomView = new CubicCustomView(this);
+        relativeLayout.addView(cubicCustomView);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
