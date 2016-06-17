0526c728f8ef8a78309da1adcdc764bbd2f2b870
Gaige <gaoqun@wiseweb.com.cn>
2016-06-17 17:37:37 +0800

    加入子线程绘制

--- customviews/app/src/main/java/custom/gq/com/customviews/CubicCustomView.java
+++ customviews/app/src/main/java/custom/gq/com/customviews/CubicCustomView.java
@@ -6,6 +6,9 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PointF;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -16,44 +19,155 @@ public class CubicCustomView extends View {
 
     private Paint mPaint;
     private int centerX, centerY;
+    private boolean flag = false;//up:true  down/false
 
-    private PointF start, end, control;
+    private PointF start, end, control, control2;
+    private Handler mHandler = new Handler(Looper.getMainLooper());
 
-    public CubicCustomView(Context context) {
+    public CubicCustomView(final Context context) {
         super(context);
+
         mPaint = new Paint();
         mPaint.setColor(Color.BLACK);
         mPaint.setStrokeWidth(8);
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setTextSize(60);
 
-        start = new PointF(0,0);
-        end = new PointF(0,0);
-        control = new PointF(0,0);
+        start = new PointF(0, 0);
+        end = new PointF(0, 0);
+        control = new PointF(0, 0);
+        control2 = new PointF(0, 0);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (control.y >= centerY * 2) {
+                        control.y = control.y - 10;
+                        if (flag) flag = false;
+                        else flag = true;
+                    } else if (control.y <= 0) {
+                        control.y = control.y + 10;
+                        if (flag) flag = false;
+                        else flag = true;
+                    } else {
+                        if (flag) {
+                            control.y = control.y - 10;
+                        } else {
+                            control.y = control.y + 10;
+                        }
+                    }
+                    control2.y = control.y;
+                    try {
+                        // 为了保证效果的同时，尽可能将cpu空出来，供其他部分使用
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                    }
+                    postInvalidate();
+                }
+            }
+        }).start();
     }
 
+    /**
+     * view current width & height
+     *
+     * @param w
+     * @param h
+     * @param oldw
+     * @param oldh
+     */
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        centerX = w/2;
-        centerY = h/2;
+        centerX = w / 2;
+        centerY = h / 2;
 
         // 初始化数据点和控制点的位置
-        start.x = centerX-200;
+        start.x = centerX - 200;
         start.y = centerY;
-        end.x = centerX+200;
+        end.x = centerX + 200;
         end.y = centerY;
-        control.x = centerX;
-        control.y = centerY-100;
+        control.x = centerX - 100;
+        control.y = centerY - 100;
+        control2.x = centerX + 100;
+        control2.y = centerY - 100;
+
+
     }
 
+    /**
+     * if your hand up the animation will return back
+     *
+     * @param event
+     * @return
+     */
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        // 根据触摸位置更新控制点，并提示重绘
-        control.x = event.getX();
+        control.x = event.getX() - 100;
         control.y = event.getY();
+        control2.x = event.getX() + 100;
+        control2.y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+
+
+                break;
+            case MotionEvent.ACTION_UP:
+//                animationReset();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            default:
+                break;
+        }
         invalidate();
+
+
         return true;
+    }
+
+    private void animationReset() {
+        while (control.x > centerX - 100 || control.x < centerX - 100 ||
+                control2.x > centerX + 100 || control2.x < centerX + 100
+                || control.y < centerY - 100 || control.y > centerY - 100
+                || control2.y < centerY - 100 || control2.y > centerY - 100) {
+
+            Log.d("control.x", control.x + "");
+            Log.d("control.y", control.y + "");
+            Log.d("control2.x", control2.x + "");
+            Log.d("control2.y", control2.y + "");
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (control.x > centerX - 100) {
+                        control.x = control.x - 1;
+                    } else if (control.x < centerX - 100) {
+                        control.x = control.x + 1;
+                    }
+
+                    if (control2.x > centerX + 100) {
+                        control2.x = control2.x - 1;
+                    } else if (control2.x < centerX + 100) {
+                        control2.x = control2.x + 1;
+                    }
+
+                    if (control.y > centerY - 100) {
+                        control.y = control.y - 1;
+                    } else if (control.y < centerY - 100) {
+                        control.y = control.y + 1;
+                    }
+
+                    if (control2.y > centerY - 100) {
+                        control2.y = control.y - 1;
+                    } else if (control2.y < centerY - 100) {
+                        control2.y = control.y + 1;
+                    }
+                    invalidate();
+                }
+            }, 1000);
+        }
     }
 
     @Override
@@ -63,23 +177,28 @@ public class CubicCustomView extends View {
         // 绘制数据点和控制点
         mPaint.setColor(Color.GRAY);
         mPaint.setStrokeWidth(20);
-        canvas.drawPoint(start.x,start.y,mPaint);
-        canvas.drawPoint(end.x,end.y,mPaint);
-        canvas.drawPoint(control.x,control.y,mPaint);
+        mPaint.setDither(true);//防抖动
+        canvas.drawPoint(start.x, start.y, mPaint);
+        canvas.drawPoint(end.x, end.y, mPaint);
+        canvas.drawPoint(control.x, control.y, mPaint);
+        canvas.drawPoint(control2.x, control2.y, mPaint);
 
-        // 绘制辅助线
+//        // 绘制辅助线
         mPaint.setStrokeWidth(4);
-        canvas.drawLine(start.x,start.y,control.x,control.y,mPaint);
-        canvas.drawLine(end.x,end.y,control.x,control.y,mPaint);
+        canvas.drawLine(start.x, start.y, control.x, control.y, mPaint);
+        canvas.drawLine(control.x, control.y, control2.x, control2.y, mPaint);
+        canvas.drawLine(control2.x, control2.y, end.x, end.y, mPaint);
 
         // 绘制贝塞尔曲线
         mPaint.setColor(Color.RED);
         mPaint.setStrokeWidth(8);
+        mPaint.setStyle(Paint.Style.FILL);
 
         Path path = new Path();
 
-        path.moveTo(start.x,start.y);
-        path.quadTo(control.x,control.y,end.x,end.y);
+        path.moveTo(start.x, start.y);
+//        path.quadTo(control.x,control.y,end.x,end.y);
+        path.cubicTo(control.x, control.y, control2.x, control2.y, end.x, end.y);
 
         canvas.drawPath(path, mPaint);
     }
